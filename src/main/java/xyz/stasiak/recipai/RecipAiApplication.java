46f52a0ab2bcf8c376b5ac24e90d2cc213ebad42
Dawid Stasiak <dawid.stasiak21@gmail.com>
2025-02-25 21:04:13 +0100

    setup project

---
+++ src/main/java/xyz/stasiak/recipai/RecipAiApplication.java
@@ -0,0 +1,13 @@
+package xyz.stasiak.recipai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/stasiak/recipai/RecipAiApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.stasiak.recipai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
